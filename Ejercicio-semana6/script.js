ead1d5ace4f558b0331edc830899af47aa568782
melissaraujo00 <140029893+melissaraujo00@users.noreply.github.com>
2024-02-27 14:40:37 -0600

    Add files via upload

---
+++ Ejercicio-semana6/script.js
@@ -0,0 +1,81 @@
+class Menu {
+    constructor(nombre, comida, cantidadComida, soda, cantidadSoda) {
+        this._nombre = nombre;
+        this._comida = comida;
+        this._cantidadComida = cantidadComida;
+        this._soda = soda;
+        this._cantidadSoda = cantidadSoda;
+    }
+
+    realizarPedido() {
+        if (isNaN(this._cantidadComida) || this._cantidadComida <= 0 || isNaN(this._cantidadSoda) || this._cantidadSoda <= 0) {
+            console.error("Por favor, ingrese cantidades válidas.");
+            return;
+        }
+
+        let totalComida = this._cantidadComida * 5;
+        let precioBebida = this.calcularPrecioBebida(this._soda);
+        let totalPedido = totalComida + (precioBebida * this._cantidadSoda);
+
+        return totalPedido;
+    }
+
+    calcularPrecioBebida(bebida) {
+        switch (bebida) {
+            case "coca-cola":
+            case "Fanta":
+                return 1;
+            case "Pepsi":
+            case "7uo":
+            case "Grepette":
+                return 0.75;
+            default:
+                return 0;
+        }
+    }
+}
+
+class Cocina extends Menu {
+    constructor(nombre, comida, cantidadComida, soda, cantidadSoda, estado) {
+        super(nombre, comida, cantidadComida, soda, cantidadSoda);
+        this._estado = estado;
+    }
+
+    marcarPedidoListoDespuesDe5Segundos() {
+        if (this._estado === "En cocina") {
+            setTimeout(() => {
+                document.getElementById('estado').innerText = "Listo";
+                alert(`Cliente ${this._nombre} ${this._cantidadComida} ordenes de ${this._comida} + ${this._cantidadSoda} Sodas de ${this._soda} su pedido está listo.`);
+            }, 5000); 
+        } else {
+            document.getElementById('estado').innerText = "En cocina";
+        }
+    }
+}
+
+function Ordenar() {
+    let nombre = document.getElementById('nombre').value;
+    let comida = document.getElementById('comida').value;
+    let cantidadComida = parseInt(document.getElementById('NumComida').value);
+    let soda = document.getElementById('soda').value;
+    let cantidadSoda = parseInt(document.getElementById('NumSoda').value);
+
+    let cocina = new Cocina(nombre, comida, cantidadComida, soda, cantidadSoda, "En cocina");
+    let totalPedido = cocina.realizarPedido();
+
+    if (typeof totalPedido !== 'undefined') {
+        document.getElementById('nom').innerText = cocina._nombre;
+        document.getElementById('tipo').innerText = cocina._comida;
+        document.getElementById('orden').innerText = cocina._cantidadComida;
+        document.getElementById('bebida').innerText = cocina._soda;
+        document.getElementById('Cantidad').innerText = cocina._cantidadSoda;
+        document.getElementById('total').innerText = totalPedido.toFixed(2);
+        document.getElementById('estado').innerText = "En cocina";
+        document.getElementById('totalprecio').innerText = totalPedido;
+
+        cocina.marcarPedidoListoDespuesDe5Segundos();
+    }
+}
+
+
+
